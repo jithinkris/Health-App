5955837fe1a7a3ea954ce3366b4902d0cca2f5a1
Jithinkrishnan P G <jithinkrishnanpg@Jithinkrishnans-MacBook-Air.local>
2026-05-28 18:31:42 +0530

    Configure Django media settings for file uploads

--- backend_django/core/urls.py
+++ backend_django/core/urls.py
@@ -20,8 +20,11 @@ from django.urls import path, include
 from django.urls import path, include
 from api.views import health
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('', health, name='health'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
